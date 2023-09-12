84870e2c6f1767c479bcd8c91e7b7fb6adc0cfbd
Juan Bogotá <juanbogota1@gmail.com>
2023-09-12 07:34:31 -0500

    actualizacion repositorio

--- EspaciosVectoriales.py
+++ EspaciosVectoriales.py
@@ -1,103 +1,172 @@
 import numpy as np
 import unittest
 
-def suma_vect(a,b):
+def suma_vect(vector1, vector2):
     'Función para la Adición de vectores complejos.'
-    m = np.array(np.zeros((1,3)),dtype = complex)
-    n = np.array(np.zeros((1,3)),dtype = complex)
-    for j in range(0,m.shape[0]):
-        for k in range(0,m.shape[1]):
-            m[j,k] = a[j,k]
-            n[j,k] = b[j,k]
-            c[j,k] = a[j,k]+b[j,k]
+    resultado = vector1 + vector2
+    return resultado
 
-    return c
+vector1 = np.array([1.+4.j, 2, 3], dtype = complex)
+vector2 = np.array([4.+1.j, 5, 6], dtype = complex)
 
-a = np.array([[1,2,3]], dtype=complex)
-b = np.array([[1,2,3]], dtype=complex)
-c = np.array(np.zeros((1,3)), dtype=complex)
+resultado = suma_vect(vector1, vector2)
 
-def inv_vect(d):
+print("1", resultado)
+
+def inv_vect(vector):
     'Función para el Inverso (aditivo) de un vector complejo.'
-    m = np.array(np.zeros((1,3)),dtype = complex)
+    inverso_aditivo = -vector
+    return inverso_aditivo
+
+vector = np.array([1 + 4j, 2, 3], dtype=complex)
+inverso_aditivo = inv_vect(vector)
+print("2", inverso_aditivo)
 
 
-    for j in range(0,m.shape[0]):
-        for k in range(0,m.shape[1]):
-            m[j,k] = d[j,k]
-            e[j,k] = (-d[j,k])
-            f[j,k] = m[j,k]+e[j,k]
-
-    return f
-
-d = np.array([[1,2,3]], dtype=complex)
-e = np.array(np.zeros((1,3)), dtype=complex)
-f = np.array(np.zeros((1,3)), dtype=complex)
-
-
-def esc_vect(z):
+def esc_vect(escalar, vector):
     'Función para la Multiplicación de un escalar por un vector complejo.'
-    m = np.array(np.zeros((1,3)),dtype=complex)
-    for j in range(0,m.shape[0]):
-        for k in range(0,m.shape[1]):
-            m[j,k]=z[j,k]*4
-    return m
+    resultado = escalar * vector
+    return resultado
 
-z = np.array([[1.+2.j,2,3]], dtype=complex)
+escalar = 2
+vector = np.array([4, 5, 6], dtype=complex)
+resultado = esc_vect(escalar, vector)
+print("3", resultado)
 
-def suma_matrix():
+def suma_matrix(matrix1, matrix2):
     'Función para la Adición de matrices complejas.'
-    a = np.array([[1.+1.j,2],[3,4.+4.j]], dtype = complex)
-    b = np.array([[9,-3],[3,5.+3.j]], dtype = complex)
-    c = a + b
+    resultado = matrix1 + matrix2
+    return resultado
 
-    return c
+matrix1 = np.array([[1,2], [3,4]], dtype=complex)
+matrix2 = np.array([[5,6], [7,8]], dtype=complex)
+resultado = suma_matrix(matrix1, matrix2)
+print("4", resultado)
 
-def inv_matrix():
+def inv_matrix(matriz):
     'Función para la Inversa (aditiva) de una matriz compleja.'
-    a = np.array([[1.+1.j,-7,-8],[1,-4,-4],[-1,3,3]], dtype = complex)
-    b = (-a)
-    c = a + b
+    inverso_aditivo = -matriz
+    return inverso_aditivo
 
-    return c
+matriz = np.array([[1 + 4j, 2, 3], [4, 5, 6]], dtype=complex)
+inverso_aditivo = inv_matrix(matriz)
+print("5", inverso_aditivo)
 
-def esc_matrix(a):
+def esc_matrix(escalar, matriz):
     'Función para la Multiplicación de un escalar por una matriz compleja.'
-    m = np.array(np.zeros((3,3)),dtype=complex)
-    for j in range(0,m.shape[0]):
-        for k in range(0,m.shape[1]):
-            m[j,k]=a[j,k]*2
-    return m
+    resultado = escalar * matriz
+    return resultado
 
-g = np.array([[1,2,3],[4,5,6],[7,8,9]], dtype=complex)
+escalar = 2
+matriz = np.array([[1 + 4j, 2, 3], [4, 5, 6]], dtype=complex)
+resultado = esc_matrix(escalar, matriz)
+print("6", resultado)
 
-def t_matrix():
+def t_matrix(matriz):
     'Función para la Transpuesta de una matriz/vector'
-    matrix = np.array([[1.+1.j,-7,-8],[1,-4,-4],[-1,3,3]], dtype = complex)
+    filas, columnas = matriz.shape
+    matriz_traspuesta = np.empty((columnas, filas), dtype=complex)
+    for j in range(filas):
+        for k in range(columnas):
+            matriz_traspuesta[k][j] = matriz[j][k]
+    return matriz_traspuesta
 
-    result = [[None for i in range(len(matrix))] for j in range(len(matrix[0]))]
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix)):
-            result[i][j] = matrix[j][i]
-    return result
+matriz = np.array([[1, 2, 3], [4 + 2j, 5, 6]], dtype=complex)
+resultado = t_matrix(matriz)
+print("7", resultado)
 
-def conj_matrix(zz):
+def conj_matrix(matriz):
     'Función para la Conjugada de una matriz/vector'
-    matriz = []
-    for i in zz:
-        fila = []
-        for k in i:
-            fila.append(k.conjugate())
-            matriz.append(fila)
+    filas, columnas = matriz.shape
+    matriz_conjugada = np.empty((filas, columnas), dtype=complex)
+    for j in range(filas):
+        for k in range(columnas):
+            elemento_original = matriz[j][k]
+            elemento_conjugado = elemento_original.real - elemento_original.imag * 1j
+            matriz_conjugada[j][k] = elemento_conjugado
+    return matriz_conjugada
 
-    return (matriz)
+matriz = np.array([[1 + 2j, 3, -4j], [5j, 5, 4]], dtype=complex)
+resultado = conj_matrix(matriz)
+print("8", resultado)
+
+def adj_matrix(matriz_compleja):
+    'Funcion para la Adjunta (daga) de una matriz/vector'
+    matriz_adjunta = t_matrix(conj_matrix(matriz_compleja))
+    return matriz_adjunta
+
+matriz_compleja = np.array([[1 + 2j, 3, -4j], [5j, 5, 4]], dtype=complex)
+resultado = adj_matrix(matriz_compleja)
+print("9", resultado)
+
+def prodc_matrix(matriz1, matriz2):
+    'Funcion para el Producto de dos matrices (de tamaños compatibles)'
+    producto = matriz1 @ matriz2
+    return producto
+
+matriz1 = np.array([[1, 2, 3], [4, 5, 6]], dtype=complex)
+matriz2 = np.array([[7, 8], [9, 10], [11, 12]], dtype=complex)
+resultado = prodc_matrix(matriz1, matriz2)
+print("10", resultado)
+
+def accion_matriz(matriz, vector):
+    'Función para calcular la "acción" de una matriz sobre un vector'
+
+    filas_matriz, columnas_matriz = matriz.shape
+    filas_vector, = vector.shape
+
+    if columnas_matriz != filas_vector:
+        return False
+
+    resultado = np.zeros((filas_matriz), dtype=complex)
+
+    for j in range(filas_matriz):
+        for k in range(columnas_matriz):
+            resultado[j] += matriz[j, k] * vector[k]
+    return resultado
+
+matriz = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]], dtype=complex)
+vector = np.array([2, 3, 4])
+resultado = accion_matriz(matriz, vector)
+print("11", resultado)
+
+def inter_vect(vector1, vector2):
+    'Funcion del Producto interno de dos vectores'
+    producto_interno = 0
+    for j in range(len(vector1)):
+        producto_interno += vector1[j] * vector2[j]
+    return producto_interno
+
+vector1 = np.array([1+2j, 2, 3], dtype=complex)
+vector2 = np.array([4, 5, 6], dtype=complex)
+resultado = inter_vect(vector1, vector2)
+print("12", resultado)
+
+def norma_vector(vector):
+    cuadrado = np.square(vector)
+    suma = np.sum(cuadrado)
+    norma = suma**0.5
+    return norma
+
+vector = np.array([1, 2, 3], dtype=complex)
+resultado = norma_vector(vector)
+print("13", resultado)
+
+def distancia_vector(vector1, vector2):
+    vector = vector1 - vector2
+    distancia = norma_vector(vector)
+    return distancia
+
+vector1 = np.array([1, -2], dtype=complex)
+vector2 = np.array([3, 4], dtype=complex)
+resultado = distancia_vector(vector1, vector2)
+print("14", resultado)
+
+#def propio_matriz
 
 
-print("Adición de vectores complejos",suma_vect(a,b))
-print("Inverso (aditivo) de un vector complejo",inv_vect(d))
-print("Multiplicación de un escalar por un vector complejo", esc_vect(z))
-print("Adición de matrices complejas", suma_matrix())
-print("Multiplicación de un escalar por una matriz compleja", esc_matrix(g))
-print("inversa aditiva matriz", inv_matrix())
-print("Transpuesta de una matriz/vector", t_matrix())
-print("Conjugada de una matriz/vector",conj_matrix(np.array([[1.+1.j,-7,-8],[1,-4,-4],[-1,3,3]], dtype = complex)))
+
+
+
+
+
--- TestEspaciosVectoriales.py
+++ TestEspaciosVectoriales.py
@@ -5,12 +5,96 @@ import unittest
 class TestCplxOperations(unittest.TestCase):
 
     def testsuma_vec(self):
-        a = np.array([[1,2,3]], dtype=complex)
-        b = np.array([[1,2,3]], dtype=complex)
-        e = [[2.+0.j,4.+0.j,6.+0.j]]
-        self.assertAlmostEqual(ev.suma_vect(a,b),e)
+        vector1 = np.array([1.+4.j, 2, 3], dtype=complex)
+        vector2 = np.array([4.+1.j, 5, 6], dtype=complex)
+        resultado_esperado = np.array([5.+5.j, 7, 9], dtype=complex)
+        resultado_real = ev.suma_vect(vector1, vector2)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
 
+    def testinv_vect(self):
+        vector = np.array([1.+4.j, 2, 3], dtype=complex)
+        resultado_esperado = np.array([-1.-4.j, -2.-0.j, -3.-0.j], dtype=complex)
+        resultado_real = ev.inv_vect(vector)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
 
+    def testesc_vect(self):
+        escalar = 2
+        vector = np.array([4, 5, 6], dtype=complex)
+        resultado_esperado = np.array([8.+0.j, 10.+0.j, 12.+0.j], dtype=complex)
+        resultado_real = ev.esc_vect(escalar, vector)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testsuma_matrix(self):
+        matrix1 = np.array([[1,2], [3,4]], dtype=complex)
+        matrix2 = np.array([[5,6], [7,8]], dtype=complex)
+        resultado_esperado = np.array([[6.+0.j, 8.+0.j], [10.+0.j, 12.+0.j]], dtype=complex)
+        resultado_real = ev.suma_matrix(matrix1, matrix2)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testinv_matrix(self):
+        matriz = np.array([[1.+4.j, 2, 3], [4, 5, 6]], dtype=complex)
+        resultado_esperado = np.array([[-1.-4.j, -2.-0.j, -3.-0.j], [-4.-0.j, -5.-0.j, -6.-0.j]], dtype=complex)
+        resultado_real = ev.inv_matrix(matriz)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testesc_matrix(self):
+        escalar = 2
+        matriz = np.array([[1 + 4j, 2, 3], [4, 5, 6]], dtype=complex)
+        resultado_esperado = np.array([[2 + 8j, 4, 6], [8, 10, 12]], dtype=complex)
+        resultado_real = ev.esc_matrix(escalar, matriz)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testt_matrix(self):
+        matriz = np.array([[1, 2, 3], [4 + 2j, 5, 6]], dtype=complex)
+        resultado_esperado = np.array([[1, 4 + 2j], [2, 5], [3, 6]], dtype=complex)
+        resultado_real = ev.t_matrix(matriz)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testconj_matrix(self):
+        matriz = np.array([[1 + 2j, 3, -4j], [5j, 5, 4]], dtype=complex)
+        resultado_esperado = np.array([[1 - 2j, 3, 4j], [-5j, 5, 4]], dtype=complex)
+        resultado_real = ev.conj_matrix(matriz)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testadj_matrix(self):
+        matriz_compleja = np.array([[1 + 2j, 3, -4j], [5j, 5, 4]], dtype=complex)
+        resultado_esperado = np.array([[1 - 2j, -5j], [3, 5], [4j, 4]], dtype=complex)
+        resultado_real = ev.adj_matrix(matriz_compleja)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testprodc_matrix(self):
+        matriz1 = np.array([[1, 2, 3], [4, 5, 6]], dtype=complex)
+        matriz2 = np.array([[7, 8], [9, 10], [11, 12]], dtype=complex)
+        resultado_esperado = np.array([[58, 64], [139, 154]], dtype=complex)
+        resultado_real = ev.prodc_matrix(matriz1, matriz2)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testaccion_matriz(self):
+        vector = np.array([2, 3, 4])
+        matriz = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]], dtype=complex)
+        resultado_esperado = np.array([20, 47, 74], dtype=complex)
+        resultado_real = ev.accion_matriz(matriz, vector)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testinter_vect(self):
+        vector1 = np.array([1+2j, 2, 3], dtype=complex)
+        vector2 = np.array([4, 5, 6], dtype=complex)
+        resultado_esperado = (32+8j)
+        resultado_real = ev.inter_vect(vector1, vector2)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testnorma_vector(self):
+        vector = np.array([1, 2, 3], dtype=complex)
+        resultado_esperado = (3.7416573867739413)
+        resultado_real = ev.norma_vector(vector)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
+
+    def testdistancia_vector(self):
+        vector1 = np.array([1, -2], dtype=complex)
+        vector2 = np.array([3, 4], dtype=complex)
+        resultado_esperado = (6.324555320336759)
+        resultado_real = ev.distancia_vector(vector1, vector2)
+        self.assertTrue(np.array_equal(resultado_real, resultado_esperado))
 
 
 if __name__ == '__main__':
